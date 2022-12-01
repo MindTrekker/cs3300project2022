e06b809a5236b86523e2923135277551acaf678e
MindTrekker <graysonmckenzie01@gmail.com>
2022-12-01 04:25:37 +0000

    Modified css for new.html.erb / Added user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "projects#new"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
