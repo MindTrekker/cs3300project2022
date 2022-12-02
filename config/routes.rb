02b5829b59f2ee878d003a487e6d3f3ddabf1c22
MindTrekker <graysonmckenzie01@gmail.com>
2022-12-02 03:56:36 +0000

    CSS changes and restructuring / added sign_in/sign_up functionality to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "projects#new"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
