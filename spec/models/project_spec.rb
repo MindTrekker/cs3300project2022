2d05e694d2fd8c3da723a8b9b0051ae65e63fc8f
MindTrekker <graysonmckenzie01@gmail.com>
2022-12-02 18:50:18 +0000

    Fixed test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe Project, type: :model do
   context "validations tests" do
-    it "ensures the description is present" do
-      project = Project.new(description: "Content of the description")
+    it "ensures the content is present" do
+      project = Project.new(title: "Test Title", description: "Content of the description")
       expect(project.valid?).to eq(true)
     end
-    
+   
     it "should be able to save project" do
       project = Project.new(title: "Title", description: "Some description content goes here")
       expect(project.save).to eq(true)
